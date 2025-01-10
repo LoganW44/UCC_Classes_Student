3afa3e056bac5fa5d9627ffa9f8ebad7d8a5cd62
LoganW44 <loganblue44@gmail.com>
2025-01-10 04:30:25 -0800

    Update hello.cpp
    Signed-off-by: LoganW44 <loganblue44@gmail.com>

--- CS_161_162_260/Week 1/hello.cpp
+++ CS_161_162_260/Week 1/hello.cpp
@@ -7,3 +7,13 @@ int main()
     cout << "Hello World!"<< endl;
     return 0;
 }
+
+int main() {
+   int gourdInput;
+
+   cin >> gourdInput; // Reads gourdInput from input
+
+   cout << "I want " << 20 << " more gourds." << endl;
+
+   return 0;
+}
